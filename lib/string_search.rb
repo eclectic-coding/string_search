31ef32437ed4c1aa917c3ef482c68698c55a82a9
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-05-12 14:51:28 -0400

    Initial commit

---
+++ lib/string_search.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "string_search/version"
+
+module StringSearch
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/string_search/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module StringSearch
+  VERSION = "0.1.0"
+end
